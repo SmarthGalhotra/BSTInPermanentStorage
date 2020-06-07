b2ab0fe9f5e252a51df5ea4ded06673eec1f8c90
SmarthGalhotra <smarthgalhotra@gmail.com>
2020-06-07 18:44:16 +0530

    Print value of a nodes searched by their keys

--- src/com/smarthgalhotra/_systemdesign/Main.java
+++ src/com/smarthgalhotra/_systemdesign/Main.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         PermanentStorageBST bst = new PermanentStorageBST();
-        bst.insertNode("Smarth","2");
-        bst.insertNode("Galhotra","100");
+//        bst.insertNode("Smarth","2");
+//        bst.insertNode("Galhotra","100");
 
+        System.out.println(bst.searchNode("Smarth"));
+        System.out.println(bst.searchNode("Galhotra"));
     }
 }
